aee38aee6794d47239741a4a9ed73024d6404756
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-08-28 22:28:46 -0400

    initial

---
+++ src/leave-tests-spec.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+/* eslint-env mocha */
+const leaveTests = require('.')
+
+describe('leave-tests', () => {
+  it('write this test', () => {
+    console.assert(leaveTests, 'should export something')
+  })
+})
